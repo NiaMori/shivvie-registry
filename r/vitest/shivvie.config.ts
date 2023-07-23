abc9c0e9406f2c616f4d5183c3979f76c6f38452
NiaMori <Hellc5261@gmail.com>
2023-07-23 22:53:04 +0800

    feat(vitest): support self-referencing module

--- r/vitest/shivvie.config.ts
+++ r/vitest/shivvie.config.ts
@@ -1,18 +1,26 @@
+import fs from 'node:fs'
 import { defineShivvie } from '@niamori/shivvie.core'
 import { z } from 'zod'
 
 export default defineShivvie({
   input: z.object({}),
 
-  async *actions({ a }) {
+  async *actions({ a, p }) {
     yield a.ni({
       names: ['vitest', '@vitest/coverage-v8', '@vitest/ui'],
       dev: true,
     })
 
+    const packageJson = z.object({
+      name: z.string(),
+    }).parse(JSON.parse(await fs.promises.readFile(p.fromTarget('package.json'), 'utf-8')))
+
     yield a.cascade({
       from: 't',
       to: '.',
+      additionalData: {
+        packageName: packageJson.name,
+      },
     })
 
     yield a.manipulate('package.json', {
