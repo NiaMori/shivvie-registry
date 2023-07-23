939f34e360be9f4071ba6545b3aecabb9deabe53
NiaMori <Hellc5261@gmail.com>
2023-07-23 23:40:35 +0800

    chore: using new eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,7 @@
+const path = require('node:path')
+
+require('node:process').env.ESLINT_TSCONFIG = path.resolve(__dirname, 'tsconfig.json')
+
 /** @type import('eslint').Linter.Config */
 module.exports = {
   root: true,
