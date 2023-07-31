91bb1e3e141e8fa400ff56976f3839b21085df76
NiaMori <Hellc5261@gmail.com>
2023-07-31 19:57:55 +0800

    fix(release): correct syncpack filter

--- r/release/shivvie.config.ts
+++ r/release/shivvie.config.ts
@@ -1,6 +1,7 @@
 import fs from 'node:fs'
 import { defineShivvie } from '@niamori/shivvie.core'
 import { z } from 'zod'
+import * as R from 'ramda'
 
 export default defineShivvie({
   input: z.object({}),
@@ -11,11 +12,22 @@ export default defineShivvie({
     if (isMonorepoRoot) {
       yield a.ni({ names: ['bumpp', 'syncpack'], dev: true })
 
+      const pkgJson = R.pipe(
+        () => fs.readFileSync(p.fromTarget('package.json'), 'utf-8'),
+        JSON.parse,
+        z.object({
+          name: z.string(),
+        }).parse,
+      )()
+
+      const scope = pkgJson.name.split('/')[0].slice(1)
+      const repo = pkgJson.name.split('/')[1].split('.')[0]
+
       yield a.manipulate('package.json', {
         path: 'package.json',
         manipulator: (pkg) => {
           pkg.scripts ||= {}
-          pkg.scripts.bump ||= 'bumpp package.json ./pkg/*/package.json --no-push --execute \'syncpack fix-mismatches --filter @niamori/shivvie.*\''
+          pkg.scripts.bump ||= `bumpp package.json ./pkg/*/package.json --no-push --execute \'syncpack fix-mismatches --filter @${scope}/${repo}.*\'`
           pkg.scripts.release ||= 'pnpm bump && pnpm build && pnpm m exec npm publish'
         },
       })
