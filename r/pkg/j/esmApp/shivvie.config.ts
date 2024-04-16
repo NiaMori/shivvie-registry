f54979117129008e628935930af84f5ea20b8f4c
NiaMori <Hellc5261@gmail.com>
2024-04-16 11:16:13 +0800

    feat(pkg): add esmApp

---
+++ r/pkg/j/esmApp/shivvie.config.ts
@@ -0,0 +1,70 @@
+import { defineShivvie } from '@niamori/shivvie.core'
+import { z } from 'zod'
+
+export default defineShivvie({
+  input: z.object({
+    scope: z.string(),
+    repo: z.string().optional(),
+    name: z.string(),
+
+    feat: z.object({
+      rollup: z.boolean().optional(),
+      eslint: z.boolean().optional(),
+      vitest: z.boolean().optional(),
+    }).optional(),
+  }),
+
+  async *actions({ i, a }) {
+    const { feat = {} } = i
+    const { rollup = true, eslint = true, vitest = true } = feat
+
+    yield a.shivvie<typeof import('@:r/package')>({
+      from: '@:r/package',
+      to: '.',
+      inputData: {
+        scope: i.scope,
+        name: i.name,
+        repo: i.repo,
+        feat: {
+          publishing: false
+        }
+      },
+    })
+
+    yield a.shivvie<typeof import('@:r/tsconfig')>({
+      from: '@:r/tsconfig',
+      to: '.',
+    })
+
+    if (rollup) {
+      yield a.shivvie<typeof import('@:r/rollup')>({
+        from: '@:r/rollup',
+        to: '.',
+      })
+    }
+
+    if (vitest) {
+      yield a.shivvie<typeof import('@:r/vitest')>({
+        from: '@:r/vitest',
+        to: '.',
+      })
+    }
+
+    if (eslint) {
+      yield a.shivvie<typeof import('@:r/eslint')>({
+        from: '@:r/eslint',
+        to: '.',
+      })
+
+      yield a.shivvie<typeof import('@:r/eslint/j/lint')>({
+        from: '@:r/eslint/j/lint',
+        to: '.',
+      })
+    }
+
+    yield a.shivvie<typeof import('@:r/monorepo/j/standalone')>({
+      from: '@:r/monorepo/j/standalone',
+      to: '.'
+    })
+  },
+})
